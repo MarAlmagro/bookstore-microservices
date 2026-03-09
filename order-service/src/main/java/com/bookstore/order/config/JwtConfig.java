79c716b9aa91510a74f33cd2299b6b46b756a578
Mar Almagro Moreno <maralmagromoreno@gmail.com>
2026-03-09 21:00:08 +0100

    fix(catalog,order): remove duplicate JwtTokenProvider bean definition

--- order-service/src/main/java/com/bookstore/order/config/JwtConfig.java
+++ order-service/src/main/java/com/bookstore/order/config/JwtConfig.java
@@ -1,14 +1,7 @@
 package com.bookstore.order.config;
 
-import com.bookstore.common.security.JwtTokenProvider;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtConfig {
-
-    @Bean
-    public JwtTokenProvider jwtTokenProvider() {
-        return new JwtTokenProvider();
-    }
 }
